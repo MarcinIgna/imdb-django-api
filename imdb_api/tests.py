912076ac3651b9879aae6bff62a8d0c6816a2b32
Marcin <mignaszak01@gmail.com>
2023-08-31 11:32:44 +0200

    user model

--- imdb_api/tests.py
+++ imdb_api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models.user_model import CustomUser
 
-# Create your tests here.
+class CustomUserTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpass'
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.email, 'testuser@example.com')
+        self.assertTrue(self.user.is_active)
+        self.assertFalse(self.user.is_staff)
+        self.assertTrue(self.user.check_password('testpass'))
