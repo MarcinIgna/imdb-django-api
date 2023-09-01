e3063caa92845e5796a3dfe7d4183bf557b25b48
Marcin <mignaszak01@gmail.com>
2023-09-01 14:37:44 +0200

    not working database update

--- IMDbDjangoAPI/settings.py
+++ IMDbDjangoAPI/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'django_extensions',
     "django.contrib.staticfiles",
     "imdb_api",
     "rest_framework",
