37d377a262ff7bdeeb5d79f6e77a9559ce9260d2
jadonblaise <chinedublaise@yahoo.com>
2023-09-01 09:24:36 +0200

    signup

--- imdb_api/urls.py
+++ imdb_api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from imdb_api.views.main import main
+from imdb_api.views.main import main, frontpage
+from imdb_api.views.signup import signup
 
 app_name = "imdb_api"
 urlpatterns = [
-    path("",main,name="home"),
+    path("",main,name="main"),
+    path("base/", frontpage, name='frontpage'),
+    path("signup/", signup, name="signup")
     
 ]
 
--- imdb_api/views/main.py
+++ imdb_api/views/main.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 
 
 def main(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def frontpage(request):
+    return render(request, 'core/frontpage.html')
