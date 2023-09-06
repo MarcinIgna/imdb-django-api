ff122afce65435278b0229aa2a63cf3ef19fedc2
Marcin <mignaszak01@gmail.com>
2023-09-06 16:13:41 +0200

    models are working

--- imdb_api/management/commands/genre_database_script.py
+++ imdb_api/management/commands/genre_database_script.py
@@ -42,39 +42,3 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('Successfully associated genres with movies.'))
 
-
-# import os
-# from django.core.management.base import BaseCommand
-# import tmdbsimple as tmdb
-# from imdb_api.models.movie_model import Movie
-# from imdb_api.models.person_model import Person
-# from imdb_api.models.genre_model import Genre
-
-# tmdb.API_KEY = os.environ.get('TMDB_API_KEY')
-
-# class Command(BaseCommand):
-#     help = 'Fetch genres from TMDb and store them in the database'
-
-#     def handle(self, *args, **kwargs):
-#         # Fetch genres from TMDb
-#         tmdb_genres = tmdb.Genres()
-#         genre_list = tmdb_genres.movie_list()['genres']
-
-#         # Create Genre objects and save them in the database
-#         for genre_info in genre_list:
-#             tmdb_id = genre_info['id']
-#             name = genre_info['name']
-
-#             Genre.objects.get_or_create(
-#                 tmdb_id=tmdb_id,
-#                 name=name
-#             )
-
-#         self.stdout.write(self.style.SUCCESS('Successfully fetched and stored genres.'))
-
-# # Retrieve all genres from the database
-# all_genres = Genre.objects.all()
-
-# # Print the list of genres
-# for genre in all_genres:
-#     print(f"ID: {genre.tmdb_id}, Name: {genre.name}")
